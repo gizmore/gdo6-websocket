ee68064f447526ce526a378b7fadde1080d70aef
gizmore <gizmore@wechall.net>
2017-11-12 15:29:21 +0100

    IPC fix

--- Server/GWS_ServerMain.php
+++ Server/GWS_ServerMain.php
@@ -47,6 +47,9 @@ include $gws->cfgWebsocketProcessorPath();
 $processor = $gws->processorClass();
 
 $server = new GWS_Server();
-$server->ipcTimer();
+if (GWF_IPC)
+{
+	$server->ipcTimer();
+}
 $server->initGWSServer(new $processor(), $gws);
 $server->mainloop($gws->cfgTimer());
