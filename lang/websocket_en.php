17fb330a407e10aed59bcd47ec47aee541bd1a93
gizmore <gizmore@wechall.net>
2018-12-17 21:56:04 +0100

    6.08-r95_fixes

--- lang/websocket_en.php
+++ lang/websocket_en.php
@@ -3,4 +3,5 @@ return array(
 'err_gws_unknown_cmd' => 'Unknwon Websocket command: %s.',
 'err_setting_validate' => 'The setting could not be saved: %s',
 'err_setting_unchanged' => 'The setting has not changed.',
+'err_bind_form' => 'Failed to bind websocket input to form: %s',
 );
