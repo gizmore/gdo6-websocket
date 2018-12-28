8ee0a62be919e9bf32e8b483d20eed532de15717
gizmore <gizmore@wechall.net>
2018-12-28 14:40:37 +0100

    6.08-r98_fixes

--- lang/websocket_en.php
+++ lang/websocket_en.php
@@ -4,4 +4,5 @@ return array(
 'err_setting_validate' => 'The setting could not be saved: %s',
 'err_setting_unchanged' => 'The setting has not changed.',
 'err_bind_form' => 'Failed to bind websocket input to form: %s',
+'err_gws_dup_code' => 'The following class uses an existing ws code: %s.',
 );
