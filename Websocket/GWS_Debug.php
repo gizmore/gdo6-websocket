8a5c9830a5b1ceca9bff37310cf6bca4379decc0
gizmore <gizmore@wechall.net>
2019-01-18 02:52:02 +0100

    6.09-r24_debug

--- Websocket/GWS_Debug.php
+++ Websocket/GWS_Debug.php
@@ -3,12 +3,15 @@ use GDO\Websocket\Server\GWS_Command;
 use GDO\Websocket\Server\GWS_Commands;
 use GDO\Websocket\Server\GWS_Message;
 use GDO\Websocket\Server\GWS_Global;
+use GDO\Perf\GDT_PerfBar;
 
 final class GWS_Debug extends GWS_Command
 {
 	public function execute(GWS_Message $msg)
 	{
-		print_r(GWS_Global::$USERS);
+		$data = GDT_PerfBar::data();
+		$data['gws_users'] = count(GWS_Global::$USERS);
+		$msg->replyText($msg->cmd(), json_encode($data));
 	}
 }
 
