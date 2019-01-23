b19c59be8703cde63b99ff86e98d034c0fae9702
gizmore <gizmore@wechall.net>
2019-01-23 01:39:32 +0100

    6-09-r28_image_rotate

--- Websocket/GWS_Debug.php
+++ Websocket/GWS_Debug.php
@@ -5,12 +5,20 @@ use GDO\Websocket\Server\GWS_Message;
 use GDO\Websocket\Server\GWS_Global;
 use GDO\Perf\GDT_PerfBar;
 
+/**
+ * A websocket command that returns performance statistics.
+ * @see GDT_PerfBar
+ * @author gizmore
+ * @since 6.08
+ * @version 6.09
+ */
 final class GWS_Debug extends GWS_Command
 {
 	public function execute(GWS_Message $msg)
 	{
 		$data = GDT_PerfBar::data();
 		$data['gws_users'] = count(GWS_Global::$USERS);
+		$data['gws_user_connections'] = count(GWS_Global::$CONNECTIONS);
 		$msg->replyText($msg->cmd(), json_encode($data));
 	}
 }
