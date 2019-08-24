18838b275c6153514a55433e740a1399be04a772
gizmore <gizmore@wechall.net>
2019-08-24 14:43:29 +0200

    6.10-r29_fix

--- lang/websocket_en.php
+++ lang/websocket_en.php
@@ -1,5 +1,6 @@
 <?php
 return array(
+'btn_connect' => 'Connect',
 'err_gws_unknown_cmd' => 'Unknwon Websocket command: %s.',
 'err_setting_validate' => 'The setting could not be saved: %s',
 'err_setting_unchanged' => 'The setting has not changed.',
